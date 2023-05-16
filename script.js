f54ab925077d2f468956912e99ace8949d684e42
Cuckss <122050270+Cuckss@users.noreply.github.com>
2023-05-16 18:47:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+let ispalindrome=true;
+	let i=0,j=str.length-1;
+	while(i<=j && ispalindrome==true){
+		if(str[i]!=str[j]){
+			ispalindrome=false;
+			break;
+		}
+		i++;
+		j--;
+	}
+	return ispalindrome;
 }
 module.exports = palindrome
