ee7982683b651cde2959f6f6741242aaca5b886f
Cuckss <122050270+Cuckss@users.noreply.github.com>
2023-05-16 18:50:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function palindrome(str){
 let ispalindrome=true;
 	let i=0,j=str.length-1;
 	while(i<=j && ispalindrome==true){
-		if(str[i]!=str[j]){
+		if(str.toLowerCase()[i]!=str.toLowerCase()[j]){
 			ispalindrome=false;
 			break;
 		}
